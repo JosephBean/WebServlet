f415d804d0af38b297841881521f5068493a288e
ShellFolder <kk1593@naver.com>
2024-06-05 10:59:42 +0900

    쿼리 스트링으로 값 받기

---
+++ src/ResultPage.java
@@ -0,0 +1,32 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ResultPage")
+public class ResultPage extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		System.out.println("이동");
+		int cnt = Integer.parseInt(request.getParameter("cnt"));
+		String txt = request.getParameter("txt");
+		PrintWriter pw = response.getWriter();
+		for(int i = 0; i < cnt; i++) {
+			System.out.println(cnt + " , " + txt);
+			pw.append((i + 1) + " "+ txt);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
